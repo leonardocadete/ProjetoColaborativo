eb0568eae995e87d31012562f38d44cfeff646ce
leonardo.cadete <leonardo.cadete@benner.com.br>
2015-11-15 11:57:34 -0200

    Projeto inicial vazio

---
+++ ProjetoColaborativo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoColaborativo.Startup))]
+namespace ProjetoColaborativo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
