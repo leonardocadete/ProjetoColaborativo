c4de8fd4036bd3a1ec55cb279da9fd13dd6bd8f6
leonardo.cadete <leonardo.cadete@benner.com.br>
2015-11-15 19:55:56 -0200

    referencias

--- ProjetoColaborativo/Startup.cs
+++ ProjetoColaborativo/Startup.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.Owin;
-using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ProjetoColaborativo.Startup))]
 namespace ProjetoColaborativo
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            //ConfigureAuth(app);
-        }
+        //ConfigureAuth(app);
     }
 }
