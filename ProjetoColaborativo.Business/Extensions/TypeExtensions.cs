a92e6bca0d45bc715ceea2f6d23cf360b257b932
leonardo.cadete <leonardo.cadete@benner.com.br>
2015-11-22 14:06:27 -0200

    Registro automatico de todos os repositorios que herdam de entidade

---
+++ ProjetoColaborativo.Business/Extensions/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjetoColaborativo.Business.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static bool IsClassOrSubclassOf<T>(this Type t)
+        {
+            return IsClassOrSubclassOf(t, typeof(T));
+        }
+
+        public static bool IsClassOrSubclassOf(Type t, Type typeToCompare)
+        {
+            if (t == null) throw new ArgumentNullException("t");
+            if (t == typeToCompare) return true;
+            if (t.BaseType == null) return false;
+            if (t.BaseType == typeToCompare) return true;
+            if (t.BaseType.IsGenericType && t.BaseType.GetGenericTypeDefinition() == typeToCompare) return true;
+
+            return IsClassOrSubclassOf(t.BaseType, typeToCompare);
+        }
+    }
+}
--- ProjetoColaborativo/Bootstrapper.cs
+++ ProjetoColaborativo/Bootstrapper.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Mvc;
 using NHibernate;
+using ProjetoColaborativo.Business.Extensions;
 using ProjetoColaborativo.Controllers;
 using ProjetoColaborativo.Models.DAO;
 using ProjetoColaborativo.Models.Entidades;
@@ -32,9 +35,26 @@ namespace ProjetoColaborativo
             container.RegisterType<ISessionFactory>(new InjectionFactory(c => SessionHelper.BuildSessionFactory()));
             container.RegisterType<ISession>(new InjectionFactory(c => SessionHelper.GetCurrentSession()));
 
-            container.RegisterType<IRepositorio<Usuario>, Repositorio<Usuario>>();
+            RegisterRepositorios(container);
 
             container.RegisterType<ITeste, Teste>();
+        }
+
+        private static void RegisterRepositorios(IUnityContainer container)
+        {
+            // Todas as classes que herdam de entidade
+            var fullList = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SelectMany(a => a.GetTypes().Where(t => t.IsClassOrSubclassOf<Entidade>()));
+
+            // Regitro automático
+            foreach (var repo in fullList)
+            {
+                var from = typeof(IRepositorio<>).MakeGenericType(repo);
+                var to = typeof(Repositorio<>).MakeGenericType(repo);
+
+                container.RegisterType(from, to);
+            }
         }
     }
 }
