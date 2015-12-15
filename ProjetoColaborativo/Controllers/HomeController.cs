dc79826664c0f4de9323667aae77ad91343410ca
leonardo.cadete <leonardo.cadete@benner.com.br>
2015-12-14 22:30:12 -0200

    Adicionado conteudo inicial do site

--- ProjetoColaborativo/Controllers/HomeController.cs
+++ ProjetoColaborativo/Controllers/HomeController.cs
@@ -12,15 +12,11 @@ namespace ProjetoColaborativo.Controllers
         [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
