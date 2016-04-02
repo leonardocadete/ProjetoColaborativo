724efa913fa9b975f26ace29f1f72e3290a4067e
leonardocadete <leonardocadete3@gmail.com>
2016-04-02 13:57:17 -0300

    Adicionado controller e diretiva para fazer o signarR

---
+++ ProjetoColaborativo/Scripts/SignalRDirective.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module('ColaborativoApp', []);
+
+    app.directive("atualizaElementos", function() {
+        return{
+            restrict: "A",
+            controller: atualizaElementosCtrl,
+            controllerAs: "ctrl"
+        };
+    });
+
+    var atualizaElementosCtrl = function () {
+        alert("olá");
+    };
+})();
